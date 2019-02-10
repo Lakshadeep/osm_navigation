654ff664e3ede669c2451bfc433f03c098f22f9e
Lakshadeep Naik <lakshadeep.naik@gmail.com>
2019-02-10 15:29:21 +0100

    Fixed error in -pi to pi wrapper function

--- semantic_localization/src/semantic_localization/sensors/wall_sides.cpp
+++ semantic_localization/src/semantic_localization/sensors/wall_sides.cpp
@@ -167,6 +167,8 @@ double WallSides::piToPi(double angle)
     angle = fmod(angle, 2 * M_PI);
     if (angle >= M_PI)
         angle -= 2 * M_PI;
+    else if (angle <= -M_PI)
+        angle += 2 * M_PI;
     return angle;
 }
 
